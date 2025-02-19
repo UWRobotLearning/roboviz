1b5a63dfe7575c03a09d69f4df46a62e297e2bb5
Rosario Scalise <rosario@cs.uw.edu>
2025-02-18 20:21:46 -0800

    update reqs

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
     name='roboviz',
     version='0.1.0',
     packages=find_packages(),
+    requires=['numpy'],
     author='Your Name',
     author_email='your.email@example.com',
     description='A brief description of your project',
