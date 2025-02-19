db374073559fbaec483b914cee07193127f67112
Rosario Scalise <rosario@cs.uw.edu>
2025-02-18 23:08:35 -0800

    update runner

--- scripts/run_streamlit.py
+++ scripts/run_streamlit.py
@@ -3,7 +3,7 @@ from roboviz.marco.process_hdf5 import extract_states, extract_one_demos
 from roboviz.marco import marco_algorithm
 
 if __name__ == "__main__":
-  states = extract_states("./expert_demos.hdf5")
-  one_demo = extract_one_demos("./expert_demos.hdf5")
+  states = extract_states("../expert_demos.hdf5")
+  one_demo = extract_one_demos("../expert_demos.hdf5")
 
   marco_algorithm.main(states, one_demo)
