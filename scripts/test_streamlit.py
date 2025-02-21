c37dcf2e1252065af3f6e508785ead14f52ad057
Rosario Scalise <rosario@cs.uw.edu>
2025-02-20 17:51:42 -0800

    test

--- scripts/test_streamlit.py
+++ scripts/test_streamlit.py
@@ -2,7 +2,7 @@ import streamlit as st
 import plotly.express as px
 
 # Create your plot
-fig = px.line(x=[1, 2, 3], y=[1, 2, 3])
+fig = px.line(x=[10, 2, 3], y=[1, 2, 3])
 
 # Display in Streamlit
 st.plotly_chart(fig)
